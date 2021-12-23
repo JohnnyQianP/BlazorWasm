5d00c3fe3fb1433942918ffbd9ce4836c8e79a68
JohnnyQianP <qianpengabc@126.com>
2021-12-23 16:29:29 +0800

    1

---
+++ src/Blazor.Wasm/Client/wwwroot/js/toinvolved.js
@@ -0,0 +1,15 @@
+﻿window.diyfunction = {
+    alertMsg: function (msg) {
+        alert(msg);
+    },
+    calldotnetfun: function () {
+        DotNet.invokeMethodAsync('Blazor.Wasm.Client', 'FromDotnetFunAsync')
+            .then(data => {
+                alert(data);
+            });
+    },
+    calldotnetfun2: function (dotNetHelper) {
+        var xx = dotNetHelper.invokeMethodAsync('FromDotnetFun2');
+        return xx
+    }
+}
